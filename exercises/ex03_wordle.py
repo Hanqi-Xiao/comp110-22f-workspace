35e0a5880c4d222e2d28809bdf44266239c4a791
Hanqi Xiao <hanqix@unc.edu>
2022-09-17 21:05:21 -0400

    made progress on ex04

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,6 +1,10 @@
-"""Budget wordle"""
+"""Budget wordle for the programmer. Does not produce a wordle block and incorrectly identifies the double yellow square edge case, cool for learning though."""
+
+__author__ = "730295059"
+
 
 def contains_char(text: str, chr: str) -> bool:
+    """Obtains if the character exists in the secret word."""
     assert len(chr) == 1
     length = len(text)
     index = 0
@@ -10,7 +14,9 @@ def contains_char(text: str, chr: str) -> bool:
         index += 1
     return False
 
+
 def emojified(guess: str, secret: str) -> str:
+    """Identify the color of each character in and produce the wordle color string."""
     assert len(guess) == len(secret)
     out_string = ""
     index = 0
@@ -21,12 +27,39 @@ def emojified(guess: str, secret: str) -> str:
             out_string += "\U0001F7E8"
         else:
             out_string += "\U00002B1C"
-        index == 1
+        index += 1
     return out_string
 
-def input_guess(guess: str, length: int) -> str:
-    while len(guess) != length:
-        guess = input(f"That was not {length} letters! Try again: ")
 
+def input_guess(length: int) -> str:
+    """Ensures input is equal in length with secret."""
+    guess = input(f"Enter a {length} character word: ")
+    while len(guess) != length:
+        guess = input(f"That wasn't {length} chars! Try again: ")
+    return guess
+
+
+def main() -> None:
+    """Entry point for the game main loop."""
+    lives: int = 6
+    secret: str = "codes"
+    length = len(secret)
+    while lives > 0:
+        turn = 7 - lives
+        print(f"=== Turn {turn}/6 ===")
+        guess = input_guess(length)
+        out = emojified(guess, secret)
+        if secret == guess:  # handles the winning edge case.
+            print(out)
+            print(f"You won in {turn}/6 turns!")
+            return
+        else:
+            print(out)
+        lives -= 1
+    print("X/6 - Sorry, try again tomorrow!")
+
+
+if __name__ == "__main__":
+    main()
 
     
---
+++ exercises/ex04_utils.py
@@ -0,0 +1,53 @@
+"""replication of classic list type utilities."""
+
+__author__ = 730295059
+
+
+def all(match: list[int], template: int) -> bool:
+    """Checks if list values match template."""
+    i = 0
+    if len(match) == 0:  # handles the edgecase of empty list
+        return False
+    while i < len(match):
+        if match[i] != template:
+            return False
+        i += 1
+    return True
+    # return False if sum([1 if i != template for i in match]) > 1 else True.
+
+
+def max(max_list: list[int]) -> int:
+    """Max function for all integers."""
+    if len(max_list) == 0:
+        raise ValueError("max() arg is an empty List")
+    my_max = max_list[0]  # does not assume the max is above 0
+    i = 0
+    while i < len(max_list):
+        if max_list[i] > my_max:
+            my_max = max_list[i]  # updates max if higher number found
+        i += 1  # iterates through max_list
+    return my_max
+
+
+def is_equal(list_1: list[int], list_2: list[int]) -> bool:
+    """Determines if the two lists numerically indistinguishable."""
+    if len(list_1) != len(list_2):  # checks if lists are equal length
+        raise ValueError("args are lists of unequal length")
+    i = 0
+    while i < len(list_1):  # uses list_i to provide lists length
+        if list_1[i] != list_2[i]:
+            return False
+        i += 1
+    return True
+
+
+def main() -> None:
+    """main"""
+    assert all([1,2,1],1) == False, "should be true"
+    assert max([1,2,123,3]) == 123, "should be 123"
+    assert is_equal([1,2,3], [1,2,3]) == True, "should be false"
+    assert is_equal([1,2,2], [1,2,3]) == False, "should be true"
+
+
+if __name__ == "__main__":
+    main()
